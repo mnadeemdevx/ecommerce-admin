1ef4e976046f0326d4ed186b78b3b6d981920b57
Muhammad Nadeem <muhammadnadeem.1596@gmail.com>
2024-06-26 02:15:25 +0500

    dashboard setup

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,37 @@
+import { redirect } from "next/navigation";
+
+import { auth } from "@clerk/nextjs/server";
+
+import db from "@/lib/db";
+
+export default async function DashboardLayout({
+    children,
+    params,
+}: {
+    children: React.ReactNode;
+    params: { storeId: string };
+}) {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect("/sign-in");
+    }
+
+    const store = await db.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId,
+        },
+    });
+
+    if (!store) {
+        redirect("/");
+    }
+
+    return (
+        <>
+            <div>This will be a navbar</div>
+            {children}
+        </>
+    );
+}
